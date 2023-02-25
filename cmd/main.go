7e9e399622b46e77d1fa2fbbcb6777cc031e7461
Atharva Shinde <atharvashinde179@gmail.com>
2023-02-25 18:19:36 +0530

    scaffold resource

--- cmd/main.go
+++ cmd/main.go
@@ -30,6 +30,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	batchv1 "my.domain/cronkapi/api/v1"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +43,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(batchv1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
